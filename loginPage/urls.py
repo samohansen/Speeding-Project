1082051e8a861e8cce3f59252dbaf009d733a4c6
benjam-rich <70960034+benjam-rich@users.noreply.github.com>
2020-11-14 16:29:37 -0700

    Now it's pretty

--- loginPage/urls.py
+++ loginPage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import loginPageView, aboutPageView ## statsPageView
+from .views import loginPageView, aboutPageView, indexPageView ## statsPageView
 
 urlpatterns = [
-    path("", loginPageView, name="index"),
     path("about/", aboutPageView, name="about"),
+    path("login/", loginPageView, name='login'),
+    path("", indexPageView, name="index"),
     # path("stats/", statsPageView, name="stats")
 ]
