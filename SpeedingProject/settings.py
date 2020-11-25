005bd25ad58c075d99e7b28f61f21fb1e443af16
Sam <samuelhansen16@gmail.com>
2020-11-25 10:51:32 -0700

    added getpass() function
    This will allow group members to use their local machine PostgreSQL database with its associated password. 'Mangofire' doesn't work on my machine unless I reconfigure my postgreSQL to use that password

--- SpeedingProject/settings.py
+++ SpeedingProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from getpass import getpass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'speedingtickets',
         'USER' : 'postgres',
-        'PASSWORD' : 'mangofire',
+        'PASSWORD' : getpass(),
         'HOST' : 'localhost'
     }
 }
